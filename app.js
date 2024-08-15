a8e84a7ebbcf6c2501ad79107b98a4a7ab7ca564
Robert Snow <rob@pop-os.localdomain>
2024-08-14 20:25:10 -0500

    Set up Express server, MongoDB connection, and initial view template

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+
+
+mongoose.connect(process.env.MONGODB_URI, {});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
